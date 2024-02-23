362431b010a019f0497a788e8c9f9a4015468fe9
Jasper Croome <jaspercroome@protonmail.com>
2024-02-23 06:09:41 -0800

    fix: remove supabase client from frontend

--- src/app/beta/list-beta.tsx
+++ src/app/beta/list-beta.tsx
@@ -5,41 +5,9 @@ import { getAllBeta } from "@/utils/queries";
 import { useQuery } from "@tanstack/react-query";
 
 const ListBeta = () => {
-  const { data } = useQuery({
-    queryKey: ["hydrate-beta"],
-    queryFn: () => getAllBeta(),
-    staleTime: 10 * 1000,
-  });
-
-  const getDate = (date: Date) => {
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDay().toString();
-    const filledDate = day.length === 2 ? day : "0" + day;
-    return `${year}-${month}-${filledDate}`;
-  };
-
-  const defaultImageString = 'https://zemcazjvgjpzzohlsgug.supabase.co/storage/v1/object/public/'
-
+  
   return (
     <div>
-      {data?.map((item) => (
-        <div key={item.id}>
-          <p className="text-4xl font-black">{item.title}</p>
-          <Image alt={item.title} src={defaultImageString+item.media_url} className="w-[200px]"/>
-          <p>{getDate(new Date(item.beta_date))}</p>
-          <div className="flex flex-row space-x-2">
-            <p className="text-xl">{item.body}</p>
-            <p>{item.gear_desc}</p>
-          </div>
-          <p className="font-black text-6xl">
-            {getStatus(item.safety_rating).icon}
-          </p>
-          <p className="font-black text-2xl">
-            {getStatus(item.safety_rating).description}
-          </p>
-        </div>
-      ))}
     </div>
   );
 };
--- src/app/beta/page.tsx
+++ src/app/beta/page.tsx
@@ -1,26 +1,9 @@
-import { useAllBetaQuery } from "@/utils/hooks";
-import { getAllBeta } from "@/utils/queries";
-import {
-  HydrationBoundary,
-  QueryClient,
-  dehydrate,
-} from "@tanstack/react-query";
-import ListBeta from "./list-beta";
 
 export default async function Beta() {
-  const queryClient = new QueryClient();
-
-  await queryClient.prefetchQuery({
-    queryKey: ["hydrate-beta"],
-    queryFn: getAllBeta,
-  });
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <HydrationBoundary state={dehydrate(queryClient)}>
         <h1>here is the beta</h1>
-        <ListBeta />
-      </HydrationBoundary>
     </main>
   );
 }
